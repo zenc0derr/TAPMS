4214e1e761fd66c8d3935a829d43beed53cc9fe2
zenc0derr <81950114+zenc0derr@users.noreply.github.com>
2022-01-09 02:45:53 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "h%qNT4KJVZ",
+  database: "TAPMS",
+});
+
+/* app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+}); */
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  //const usertype = req.body.usertype;
+
+  db.query(
+    "select Username, Passwords from Registration where Username = ? and Passwords = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send(err);
+      }
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send({ msg: "Hi" });
+      }
+    }
+  );
+});
+
+app.get("/students", (req, res) => {
+  db.query("SELECT * FROM Students_Personal_Database", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    "UPDATE employees SET wage = ? WHERE id = ?",
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
+});
